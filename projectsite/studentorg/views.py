e907c7730c03573721d6b5a003f77ef9355e3c07
saisaitablatin <sairahtablatin@gmail.com>
2024-05-26 16:06:05 +0800

    Home Html

--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -24,6 +24,16 @@ class HomePageView(ListView):
     model = Organization
     context_object_name = "home"
     template_name = "home.html"
+    class ChartView(ListView):
+    template_name = 'chart.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+    def get_queryset(self, *args, **kwargs):
+        pass
+
 
 
 class OrganizationList(ListView):
